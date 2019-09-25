91da88a0321a1bbe2647b1d51c24b3fe5eea4a1a
qinhan <qinhan@xindong.com>
2019-09-25 18:40:26 +0800

    feat(build): refactor build.sh

--- pack/build.sh
+++ pack/build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-pyinstaller -F main.py
+#建立虚拟环境
+pipenv install
+#进入虚拟环境
+pipenv shell
+#安装模块
+pip install 小工具.py里面用到的模块
+#打包的模块也要安装
+pip install pyinstaller
+#开始打包
+pyinstaller -F ./main.py
