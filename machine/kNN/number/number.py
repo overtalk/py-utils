3550ce5fa37cfdfa991e580eee839ec00d4e0618
qinhan <qinhan@xindong.com>
2019-09-26 17:52:17 +0800

    feat(all): del usless __init__.py

--- machine/kNN/number/number.py
+++ machine/kNN/number/number.py
@@ -1,29 +1,8 @@
+import sys
+sys.path.append('..')
 from numpy import *
 from os import listdir
-import operator
-
-
-def classify(in_x: list, data_set: mat, labels: list, k: int):
-    # shape : 矩阵或者数组取其行数，获得样本数量
-    data_set_size = data_set.shape[0]
-    # tile : 重复，tile(in_x, (data_set_size, 1))的含义就是将in_X重复样本数量份
-    # 再减去data_set，就可以得到每个属性的差值
-    diff_mat = tile(in_x, (data_set_size, 1)) - data_set
-    # 平方
-    sq_diff_mat = diff_mat ** 2
-    # 所有距离平方求和
-    sq_distances = sq_diff_mat.sum(axis=1)
-    # 开方得到距离
-    distances = sq_distances ** 0.5
-    # 生序排列
-    sorted_distances = distances.argsort()
-    class_count = {}
-    for i in range(k):
-        vote_i_label = labels[sorted_distances[i]]
-        class_count[vote_i_label] = class_count.get(vote_i_label, 0) + 1
-    sorted_class_count = sorted(class_count.items(),
-                                key=operator.itemgetter(1), reverse=True)
-    return sorted_class_count[0][0]
+from dating.demo import classify
 
 
 def img2vector(file_name: str):
